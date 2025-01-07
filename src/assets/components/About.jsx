c56f08229829af7edf433843cc745a7b2b081b84
Ismaheel <oladimeji2312@gmail.com>
2025-01-07 23:53:57 +0100

    new commit

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -6,7 +6,7 @@ const About = ()=>{
              from the University of Ibadan. I am looking for exciting opportunities <br /> where I can showcase my skills and continue to learn. <br />Passionate and curious about solving critical problems, 
              <br />while focusing on interactivity and accessibility when coding. 
              <br />I'm currently exploring content writing and digital marketing <br />
-             when I'm not coding. I enjoy coding, playing football, writing and travelling.</p></h3><br />
+             when I am not coding. I enjoy coding, playing football, writing and travelling.</p></h3><br />
             <a href="http://bit.ly/3YBvNdX"><button>Download my CV</button></a></div>
 
             <div><img src={Image1} alt="" /></div>
